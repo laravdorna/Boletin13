a946f7291b3f8aca5dd775b0175aa8f74a24b819
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-12-04 11:10:12 +0100

    1.creadas clases

---
+++ src/boletin13_futbol/Boletin13_Futbol.java
@@ -0,0 +1,24 @@
+/*
+
+Crear un proxecto que simule o comportamento dunha selección de futbol .
+Para iso definir 3 clases que representan a obxectos xogador,
+adestrador e masaxista .
+
+ */
+
+package boletin13_futbol;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Boletin13_Futbol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
